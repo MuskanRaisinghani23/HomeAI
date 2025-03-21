538de8ee0bcef6a7f23be34fb1decea98c8b8adc
Muskan Raisinghani <raisinghani.m@northeastern.edu>
2025-03-21 00:49:49 -0400

    Pipeline for marketplace complete (#20)
    * airflow with spark
    
    * merge query updated

---
+++ airflow/dags/scripts/facebook/load_data.py
@@ -0,0 +1,103 @@
+import configparser
+from helper.snowflake_helper import snowflake_connection
+import os
+
+config = configparser.ConfigParser()
+config.read('configuration.properties')
+
+def load_data(file_name, stage_name):
+  try:
+    conn = snowflake_connection()
+    cur = conn.cursor()
+    put_command = f"PUT file://{file_name} @{stage_name} AUTO_COMPRESS=TRUE OVERWRITE=TRUE"
+    print(put_command)
+    result = cur.execute(put_command).fetchall()
+    print("PUT command result: ", result)
+    print(f"Data loaded successfully from {file_name} to snowflake stage")
+  except Exception as e:
+    print("Exception in load_data function: ",e)
+    return
+
+def merge_data(stage_name, table_name, filename, primary_key):
+  try:
+    conn = snowflake_connection()
+    cur = conn.cursor()
+
+    merge_query = f'''
+    MERGE INTO {table_name} AS t
+    USING (SELECT 
+                parse_json($1):url LISTING_URL,
+                parse_json($1):location LOCATION,
+                parse_json($1):price PRICE,
+                parse_json($1):room_count ROOM_COUNT,
+                parse_json($1):bath_count BATH_COUNT,
+                parse_json($1):people_count PEOPLE_COUNT,
+                parse_json($1):description DESCRIPTION_SUMMARY,
+                parse_json($1):contact CONTACT,
+                parse_json($1):laundry_available LAUNDRY_AVAILABLE,
+                parse_json($1):img IMAGE_URL,
+                parse_json($1):room_type ROOM_TYPE,
+                parse_json($1):other_details OTHER_DETAILS
+            FROM @{stage_name}/{filename}.gz (FILE_FORMAT => JSON_FORMAT)) AS s    
+    ON t.{primary_key} = s.{primary_key} 
+    WHEN NOT MATCHED THEN 
+        INSERT (listing_url,
+                location,
+                price,
+                listing_date, 
+                room_count,
+                bath_count,
+                people_count,
+                description_summary,
+                source ,
+                contact,
+                laundry_available,
+                report_count,
+                image_url,
+                room_type,
+                other_details)
+        VALUES (s.LISTING_URL,
+                s.LOCATION,
+                CAST(NULLIF(REGEXP_REPLACE(s.PRICE, '[^0-9.]', ''), '') AS NUMBER),
+                CURRENT_DATE,
+                CAST(NULLIF(s.ROOM_COUNT, '') AS NUMBER),
+                CAST(NULLIF(s.BATH_COUNT, '') AS NUMBER),
+                CAST(NULLIF(s.PEOPLE_COUNT, '') AS NUMBER),
+                s.DESCRIPTION_SUMMARY,
+                'MARKETPLACE',
+                s.CONTACT,
+                s.LAUNDRY_AVAILABLE,
+                0,
+                s.IMAGE_URL,
+                s.ROOM_TYPE,
+                s.OTHER_DETAILS);
+    '''
+    print(merge_query)
+    print("merging data...")
+
+    # executing load
+    cur.execute(merge_query)
+    
+    print(f"Data merged successfully from {stage_name} to {table_name}")
+    
+    # Close the cursor and connection
+    cur.close()
+    conn.close()
+      
+  except Exception as e:
+    print("Exception in merge_data function: ",e)
+    return
+  
+  
+def load_to_snowflake():
+  current_directory = os.path.dirname(os.path.realpath(__file__))
+  print("path +++++++++++++++++", current_directory)
+  file_name = 'data/facebook/facebook_listing_transformed_cleaned.json'
+  stage_file_name = 'facebook_listing_transformed_cleaned.json'
+  marketplace_stage_name = 'MARKETPLACE_LISTING_STAGE'
+  room_table_name = 'rooms_listings'
+
+  # marketplace data load
+  load_data(file_name, marketplace_stage_name)
+
+  merge_data(marketplace_stage_name, room_table_name, stage_file_name, 'LISTING_URL')
---
+++ airflow/dags/spark/facebook/extract_listing.py
@@ -0,0 +1,106 @@
+import os 
+import logging
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from selenium.webdriver.chrome.options import Options
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("airflow.task")
+
+def extract_listing_for_url(url):
+    """
+    Scrape the listing for the given URL
+    """
+    try:
+        logger.info(f"Processing URL: {url}")
+
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Run in headless mode
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        browser = Browser('chrome', options=chrome_options)
+        browser.visit(url)
+
+        # Handle pop-ups
+        if browser.is_element_present_by_css('div[aria-label="Close"]', wait_time=5):
+            logger.info("Closing pop-up")
+            browser.find_by_css('div[aria-label="Close"]').first.click()
+
+        # Click the "See More" button if it exists
+        try:
+            see_more_button = browser.find_by_css('div[role="button"] span.x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x6prxxf.xvq8zen.x1s688f.xzsf02u')
+            if see_more_button:
+                see_more_button.click()
+            else:
+                logger.info("See more button not found")
+        except Exception as e:
+            logger.warning(f"See more button error: {e}")
+
+        # Parse the HTML
+        html = browser.html
+        market_soup = soup(html, 'html.parser')
+
+        # Extract relevant data
+        div_element = market_soup.find('div', class_='xckqwgs x26u7qi x2j4hbs x78zum5 xnp8db0 x5yr21d x1n2onr6 xh8yej3 xzepove x1stjdt1')
+        div_text = div_element.get_text(separator=' ', strip=True) if div_element else ''
+
+        # Close browser
+        browser.quit()
+        
+        return div_text
+
+    except Exception as e:
+        logger.error(f"Error processing {url}: {e}")
+        return None
+
+def main():
+    # Initialize Spark session
+    spark = SparkSession.builder.appName("ListingExtraction").getOrCreate()
+    extract_listing_udf = udf(extract_listing_for_url, StringType())
+
+    # Step 1: Read CSV using Pandas
+    csv_path = "data/facebook/facebook_metadata_raw.csv"
+    if not os.path.exists(csv_path):
+        logger.error(f"CSV file {csv_path} not found!")
+        return
+
+    df_pandas = pd.read_csv(csv_path)
+    logger.info("CSV file loaded successfully.")
+    
+    print(df_pandas.head())
+
+    # Convert Pandas DataFrame to Spark DataFrame
+    df_spark = spark.createDataFrame(df_pandas)
+
+    # Step 2: Apply UDF to Extract Metadata
+    df_spark = df_spark.withColumn("details", extract_listing_udf(df_spark["url"]))
+
+    # Step 3: Convert Back to Pandas
+    df_pandas_transformed = df_spark.toPandas()
+
+    print(df_pandas_transformed.head())
+
+    # Step 4: Save Processed Data
+    output_path = "data/facebook/facebook_listing.json"
+    df_pandas_transformed.to_json(output_path, orient="records")
+
+    # Step 5: Delete Old CSV File
+    if os.path.exists(csv_path):
+        os.remove(csv_path)
+        logger.info(f"Old CSV file {csv_path} deleted.")
+
+    logger.info("Listing extraction completed and saved to JSON.")
+
+    # Stop Spark session
+    spark.stop()
+
+# Ensure the script runs only when executed directly
+if __name__ == "__main__":
+    main()
